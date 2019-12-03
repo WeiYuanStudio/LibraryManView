7d1949b9bee996c612bf64adde8225ed9e3de7a9
WeiYuanStudio <WeiYuanStudio@gmail.com>
2019-12-03 11:45:44 +0800

    :sparkles: 完善主页搜索，增加开发配置文件
    完善主页搜索框的搜索路由
    增加了一个配置js文件，方便调试

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -1,37 +1,58 @@
 <template>
-  <div id="search-bar">
-    <form id="query-panel">
-      <input id="query-input" type="text" :placeholder="holder_info" />
-      <input id="query-submit" type="submit" value="Search" />
-    </form>
-  </div>
+    <div id="search-bar">
+        <form id="query-panel">
+            <input id="query-input" type="text" v-bind:placeholder="holder_info" v-model="title" />
+            <input id="query-submit" type="submit" value="Search" v-on:click="search_book" />
+        </form>
+    </div>
 </template>
+
 <script>
+import axios from 'axios';
+import conf from '@/dev-config'
+
 export default {
-  name: "search-bar",
-  data: function() {
-    return {
-      holder_info: "Search your book title"
-    };
-  }
+    name: "search-bar",
+    data: function() {
+        return {
+            holder_info: "Search your book by title",
+            title: ""
+        };
+    },
+    methods: {
+        update_motto() {
+            axios
+                .get(conf.api_path() + '/api/motto')
+                .then(response => (this.holder_info = response.data));
+        },
+        search_book() {
+            this.$router.push({
+                path: './books',
+                query: { title: this.title }
+            });
+        }
+    },
+    mounted() {
+        this.update_motto();
+    }
 };
 </script>
 <style scoped lang="scss">
 $base-width: 0.8; //搜索框总长度
 
 input {
-  padding: 10px 5px;
-  border-radius: 5px;
-  border: solid 2px gray;
-  margin: 5px;
-  -webkit-appearance: none;
+    padding: 10px 5px;
+    border-radius: 5px;
+    border: solid 2px gray;
+    margin: 5px;
+    -webkit-appearance: none;
 }
 
 #query-input {
-  width: 80% * $base-width;
+    width: 80% * $base-width;
 }
 
 #query-submit {
-  width: 20% * $base-width;
+    width: 20% * $base-width;
 }
 </style>
---
+++ src/dev-config.js
@@ -0,0 +1,5 @@
+export default {
+    api_path: function() {
+        return 'http://127.0.0.1:5000' //方便开发时后端联调
+    }
+}
